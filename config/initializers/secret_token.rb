b53f7e850d664c17930f28c23f7b244c3a25024c
ipshow <ipshow@gmail.com>
2013-03-07 16:10:18 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '0b3da09b3c2c5fbc414622f68321d56816b4458464f0810ac8c0e2181b6241572d5429b7c8e21cc7faff5e1d9006f24961d08f11277a1e387153c79db6f3131a'
