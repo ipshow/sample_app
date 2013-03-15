07e5c483165a685449f9950d4604d6075c219939
ubuntu <ipshow@msn.com>
2013-03-15 22:12:59 +0800

    next-9.3.2

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -21,5 +21,28 @@ module SessionsHelper
     	@current_user ||= User.find_by_remember_token(cookies[:remember_token])
   	end
 
+    def current_user?(user)
+      user == current_user
+    end
+
+    def redirect_back_or(default)
+      redirect_to(session[:return_to] || default)
+      session.delete(:return_to)
+    end
+
+    def store_location
+      session[:return_to] = request.fullpath
+    end
+
+    def create
+      user = User.find_by_email(params[:session][:email])
+    if user && user.authenticate(params[:session][:password])
+      sign_in user
+      redirect_back_or user
+    else
+      flash.now[:error] = 'Invalid email/password combination'
+      render 'new'
+    end
+  end
 
 end
